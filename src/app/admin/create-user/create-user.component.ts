95c652014b674dd45e8cbf9b2763d34a5531bdf0
ShrawanDadarwa <shrawan.dadarwal@neosoftmail.com>
2024-03-28 19:52:50 +0530

    development work

--- src/app/admin/create-user/create-user.component.ts
+++ src/app/admin/create-user/create-user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { eventNames } from 'process';
 
 @Component({
   selector: 'app-create-user',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './create-user.component.css'
 })
 export class CreateUserComponent {
+  userName:string="shrawan Kumar";
+  link:string="www.google.com";
+  imagelink:string='assets/pick.jpg';
+  status:boolean=false;
+  buttonClass:string='buttonBack';
+  OnMouseClick(event:any){
+    this.buttonClass='buttonColor';
+    this.status=true;
+    console.log(event.eventNames);
+  }
+  OnDoubleClick(event:any){
+    this.buttonClass='buttonBack';
+    this.status=false;
+  }
 
+  OnKeyUp(event:any){
+    console.log(event.key);
+  }
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { CreateUserComponent } from './admin/create-user/create-user.component';
 
 
 export const routes: Routes = [
@@ -6,6 +7,10 @@ export const routes: Routes = [
     {
         path: 'admin',
         loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+    },{
+        path:'create-user',
+        loadComponent: () => import('./admin/create-user/create-user.component').then(c=>c.CreateUserComponent)
+        // component:CreateUserComponent
     }
    
 ];
